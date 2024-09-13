35bf9b6af234b9653afca5846b2139e3d0c56e51
tinsae830 <amantinsuhabtamu@gmail.com>
2024-09-13 18:34:23 +0300

    Added a search functionality using the API

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
+
+function refreshWeather(response){
+    let currentTemperature = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city
+    currentTemperature.innerHTML = Math.round(temperature);
+
+}
+
+
+
+
+
+function searchCity(city){
+    let apiKey = "4fa78to10b1c43d3810fccabf38a2f36"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric` 
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#form-search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
   }
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+  searchCity("Addis Ababa");
