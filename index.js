bb5249bfa069444fa58b0cefa77362487b197f56
tinsae830 <amantinsuhabtamu@gmail.com>
2024-09-14 20:23:03 +0300

    Injected more weather data

--- index.js
+++ index.js
@@ -3,9 +3,41 @@ function refreshWeather(response){
     let currentTemperature = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time*1000);
+
     cityElement.innerHTML = response.data.city
     currentTemperature.innerHTML = Math.round(temperature);
 
+    descriptionElement.innerHTML = response.data.condition.description;
+
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
+
+
+    timeElement.innerHTML = formatDate(date);
+
+
+}
+
+
+function formatDate(date){
+
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursady", "Friday", "Saturday"]
+
+let day = days[date.getDay()];
+
+if(minutes<10){
+    minutes = `0${minutes}`
+}
+
+return `${day} ${hours}:${minutes}`
 }
 
 
