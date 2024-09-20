1fd383862caf1a7f7b370326feea96cb86ddaa46
tinsae830 <amantinsuhabtamu@gmail.com>
2024-09-21 01:20:22 +0300

    Updated the forecast HTML from javascript

--- index.js
+++ index.js
@@ -62,40 +62,50 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#form-search-input");
     searchCity(searchInput.value);
   }
+
+  function formatDay(timestamp){
+    let date = new Date (timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+ 
+    return days[date.getDay()];
+}
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 function getForecast(city){
 let apiKey = "4fa78to10b1c43d3810fccabf38a2f36";
-let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`
 axios(apiUrl).then(displayForecast);
 }
 
 
   function displayForecast(response) {
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
   
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if(index < 5){
       forecastHtml =
         forecastHtml +
         `
         <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">🌤️</div>
+          <div class="weather-forecast-date">${formatDay(day.time)}</div>
+          
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+          
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15º</strong>
+              <strong>${Math.round(day.temperature.maximum)}º</strong>
             </div>
-            <div class="weather-forecast-temperature">9º</div>
+            <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}º</div>
           </div>
         </div>
       `;
+        }
     });
   
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
   }
   searchCity("Addis Ababa");
-  displayForecast();
+  //displayForecast();
